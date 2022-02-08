7a7a18feb1d59aba1fb0c93335e533391f1f72e0
fiona-deriv <fiona@deriv.com>
2022-02-08 12:18:38 +0200

    add files

--- components/InputPage.jsx
+++ components/InputPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
-import Card from './card';
+import React, { useEffect, useState } from 'react'
+import Card from './card'
+import vCardsJS from 'vcards-js'
 
 const MyForm = () => {
     const [firstName, setName] = useState('')
@@ -15,6 +15,7 @@ const MyForm = () => {
     const [stateName, setstateName] = useState('')
     const [countryName, setcountryName] = useState('')
     const [websiteName, setwebsiteName] = useState('')
+    const [qrCodeUrl, setQrCode] = useState('')
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -30,23 +31,38 @@ const MyForm = () => {
         The name you entered is ${stateName}
         The name you entered is ${countryName}
         The name you entered is ${websiteName}
-        `);
+        `)
     }
+    const vCard = vCardsJS()
 
+    vCard.firstName = { firstName }
+    vCard.lastName = { lastName }
+    vCard.workPhone = { mobileName }
+
+    // get as formatted string and encode it
+    const vCardString = vCard.getFormattedString()
+    const vCardEncodedString = encodeURIComponent(vCardString)
+
+    useEffect(() => {
+        setQrCode(
+            `https://chart.googleapis.com/chart?chs=300x300&choe=UTF-8&chld=M%7C0&cht=qr&chl=${vCardEncodedString}&companyname=${companyName}`,
+        )
+    }, [companyName])
     return (
         <div className="container_box">
-            <div className='card_container'>
-                <Card 
+            <div className="card_container">
+                <Card
                     id="front_side"
-                    firstName={firstName} 
-                    lastName={lastName} 
+                    firstName={firstName}
+                    lastName={lastName}
                     email={emailName}
                     company={companyName}
                     mobile={mobileName}
                     fax={faxName}
                     front={true}
+                    qrcode={<img src={qrCodeUrl} className="QR_code" />}
                 />
-                <Card 
+                <Card
                     id="back_side"
                     country={countryName}
                     city={cityName}
@@ -54,7 +70,6 @@ const MyForm = () => {
                     street={streetName}
                     zip={zipName}
                     website={websiteName}
-
                 />
             </div>
             <div className="content form_container">
@@ -77,9 +92,7 @@ const MyForm = () => {
                         onChange={(e) => setlastName(e.target.value)}
                         placeholder=" Last name"
                     />
-
                     <label>Contact </label>
-
                     <input
                         type="number"
                         name="mobile"
@@ -95,7 +108,6 @@ const MyForm = () => {
                         onChange={(e) => setfaxName(e.target.value)}
                         placeholder="Fax"
                     />
-
                     <label>Email </label>
                     <input
                         type="email"
@@ -105,16 +117,13 @@ const MyForm = () => {
                         onChange={(e) => setemailName(e.target.value)}
                         placeholder="Your@email.com"
                     />
-
                     <label>Company </label>
                     <input
                         type="text"
                         name="company"
                         value={companyName}
                         onChange={(e) => setcompanyName(e.target.value)}
-                        placeholder="Company"
                     />
-
                     <label>Address </label>
                     <input
                         type="text"
@@ -126,7 +135,6 @@ const MyForm = () => {
                         maxLength={50}
                         placeholder="Address"
                     />
-
                     <label>City </label>
                     <input
                         type="text"
@@ -144,7 +152,6 @@ const MyForm = () => {
                         onChange={(e) => setzipName(e.target.value)}
                         placeholder="ZIP"
                     />
-
                     <label>State </label>
                     <input
                         type="text"
@@ -154,7 +161,6 @@ const MyForm = () => {
                         onChange={(e) => setstateName(e.target.value)}
                         placeholder="State"
                     />
-
                     <label>Country </label>
                     <input
                         type="text"
@@ -164,7 +170,6 @@ const MyForm = () => {
                         onChange={(e) => setcountryName(e.target.value)}
                         placeholder="Country"
                     />
-
                     <label>Website</label>
                     <input
                         type="text"
@@ -173,7 +178,6 @@ const MyForm = () => {
                         onChange={(e) => setwebsiteName(e.target.value)}
                         placeholder="www.your-website.com"
                     />
-
                     <div className="button-content">
                         <button className="btn-container" type="submit">
                             <span> Submit</span>
@@ -184,5 +188,4 @@ const MyForm = () => {
         </div>
     )
 }
-
-export default MyForm;
+export default MyForm
